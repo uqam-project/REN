1ed7f3632e5620e588a52737abeebfcf3e38cd87
uqam-project <65499544+uqam-project@users.noreply.github.com>
2020-06-21 12:37:47 -0400

    Update main.py

--- models/model2/main.py
+++ models/model2/main.py
@@ -139,19 +139,19 @@ def model_fn(features, labels, mode, params):
     layers.append(char_embeddings)
     layers.append(output)
     
-    lm_embeddings = tf.concat(
+    wsum_embeddings = tf.concat(
                               [tf.expand_dims(t, axis=1) for t in layers], axis=1)
     
     weights = tf.sequence_mask(nwords)
     
 
-    bilm_ops = {'lm_embeddings':lm_embeddings,
+    wsum_ops = {'wsum_embeddings':wsum_embeddings,
                 'mask': weights}
     
-    weight_sum = weight_layers(
-        'elmo_input1', bilm_ops, l2_coef=1.0, do_layer_norm=True, use_top_only=False)    
+    weighted_sum = weight_layers(
+        'elmo_input1', wsum_ops, l2_coef=1.0, do_layer_norm=True, use_top_only=False)    
                                      
-    output = tf.layers.dropout(weight_sum['weighted_op'], rate=dropout, training=training)    
+    output = tf.layers.dropout(weighted_sum['weighted_op'], rate=dropout, training=training)    
 
     
     
