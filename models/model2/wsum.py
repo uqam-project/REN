d04d9db479bdf622079e6eeb9c78c6eb421fc76a
uqam-project <65499544+uqam-project@users.noreply.github.com>
2020-06-21 11:52:10 -0400

    Update wsum.py

--- models/model2/wsum.py
+++ models/model2/wsum.py
@@ -1,22 +1,44 @@
 import tensorflow as tf
 
-def weight_sum(name, wsum_ops, l2_coef=None,
+def weight_layers(name, bilm_ops, l2_coef=None,
                   use_top_only=False, do_layer_norm=False):
-
+    '''
+    Weight the layers of a biLM with trainable scalar weights to
+    compute ELMo representations.
+    For each output layer, this returns two ops.  The first computes
+        a layer specific weighted average of the biLM layers, and
+        the second the l2 regularizer loss term.
+    The regularization terms are also add to tf.GraphKeys.REGULARIZATION_LOSSES 
+    Input:
+        name = a string prefix used for the trainable variable names
+        bilm_ops = the tensorflow ops returned to compute internal
+            representations from a biLM.  This is the return value
+            from BidirectionalLanguageModel(...)(ids_placeholder)
+        l2_coef: the l2 regularization coefficient $\lambda$.
+            Pass None or 0.0 for no regularization.
+        use_top_only: if True, then only use the top layer.
+        do_layer_norm: if True, then apply layer normalization to each biLM
+            layer before normalizing
+    Output:
+        {
+            'weighted_op': op to compute weighted average for output,
+            'regularization_op': op to compute regularization term
+        }
+    '''
     def _l2_regularizer(weights):
         if l2_coef is not None:
             return l2_coef * tf.reduce_sum(tf.square(weights))
         else:
             return 0.0
 
-    # Get ops for computing embeddings and mask
-    wsum_embeddings = wsum_ops['wsum_embeddings']
-    mask = wsum_ops['mask']
+    # Get ops for computing LM embeddings and mask
+    lm_embeddings = bilm_ops['lm_embeddings']
+    mask = bilm_ops['mask']
 
-    n_layers = int(wsum_embeddings.get_shape()[1])
-    lm_dim = int(wsum_embeddings.get_shape()[3])
+    n_lm_layers = int(lm_embeddings.get_shape()[1])
+    lm_dim = int(lm_embeddings.get_shape()[3])
 
-    with tf.control_dependencies([wsum_embeddings, mask]):
+    with tf.control_dependencies([lm_embeddings, mask]):
         # Cast the mask and broadcast for layer use.
         mask_float = tf.cast(mask, 'float32')
         broadcast_mask = tf.expand_dims(mask_float, axis=-1)
@@ -33,15 +55,15 @@ def weight_sum(name, wsum_ops, l2_coef=None,
             )
 
         if use_top_only:
-            layers = tf.split(wsum_embeddings, n_layers, axis=1)
+            layers = tf.split(lm_embeddings, n_lm_layers, axis=1)
             # just the top layer
             sum_pieces = tf.squeeze(layers[-1], squeeze_dims=1)
             # no regularization
             reg = 0.0
         else:
             W = tf.get_variable(
-                '{}_W'.format(name),
-                shape=(n_layers, ),
+                '{}_ELMo_W'.format(name),
+                shape=(n_lm_layers, ),
                 initializer=tf.zeros_initializer,
                 regularizer=_l2_regularizer,
                 trainable=True,
@@ -49,10 +71,10 @@ def weight_sum(name, wsum_ops, l2_coef=None,
 
             # normalize the weights
             normed_weights = tf.split(
-                tf.nn.softmax(W + 1.0 / n_layers), n_layers
+                tf.nn.softmax(W + 1.0 / n_lm_layers), n_lm_layers
             )
             # split LM layers
-            layers = tf.split(wsum_embeddings, n_layers, axis=1)
+            layers = tf.split(lm_embeddings, n_lm_layers, axis=1)
     
             # compute the weighted, normalized LM activations
             pieces = []
@@ -67,21 +89,21 @@ def weight_sum(name, wsum_ops, l2_coef=None,
             reg = [
                 r for r in tf.get_collection(
                                 tf.GraphKeys.REGULARIZATION_LOSSES)
-                if r.name.find('{}_W/'.format(name)) >= 0
+                if r.name.find('{}_ELMo_W/'.format(name)) >= 0
             ]
             if len(reg) != 1:
                 raise ValueError
 
-        # scale the weighted sum by delta
-        delta = tf.get_variable(
-            '{}_delta'.format(name),
+        # scale the weighted sum by gamma
+        gamma = tf.get_variable(
+            '{}_ELMo_gamma'.format(name),
             shape=(1, ),
             initializer=tf.ones_initializer,
             regularizer=None,
             trainable=True,
         )
-        weighted_layers = sum_pieces * delta
+        weighted_lm_layers = sum_pieces * gamma
 
-        ret = {'weighted_op': weighted_layers, 'regularization_op': reg}
+        ret = {'weighted_op': weighted_lm_layers, 'regularization_op': reg}
 
     return ret
