d863c5d81876b814f746b8a78a965d6e9709f8c8
uqam-project <65499544+uqam-project@users.noreply.github.com>
2020-06-21 12:46:22 -0400

    Update wsum.py

--- models/model2/wsum.py
+++ models/model2/wsum.py
@@ -9,7 +9,7 @@ def weight_layers(name, wsum_ops, l2_coef=None,
         else:
             return 0.0
 
-    # Get ops for computing LM embeddings and mask
+    # Get ops for computing embeddings and mask
     wsum_embeddings = wsum_ops['wsum_embeddings']
     mask = wsum_ops['mask']
 
@@ -40,7 +40,7 @@ def weight_layers(name, wsum_ops, l2_coef=None,
             reg = 0.0
         else:
             W = tf.get_variable(
-                '{}_ELMo_W'.format(name),
+                '{}_wsum_W'.format(name),
                 shape=(n_layers, ),
                 initializer=tf.zeros_initializer,
                 regularizer=_l2_regularizer,
@@ -51,10 +51,10 @@ def weight_layers(name, wsum_ops, l2_coef=None,
             normed_weights = tf.split(
                 tf.nn.softmax(W + 1.0 / n_layers), n_layers
             )
-            # split LM layers
+            # split embeddings layers
             layers = tf.split(wsum_embeddings, n_layers, axis=1)
     
-            # compute the weighted, normalized LM activations
+            # compute the weighted, normalized embeddings activations
             pieces = []
             for w, t in zip(normed_weights, layers):
                 if do_layer_norm:
@@ -67,21 +67,21 @@ def weight_layers(name, wsum_ops, l2_coef=None,
             reg = [
                 r for r in tf.get_collection(
                                 tf.GraphKeys.REGULARIZATION_LOSSES)
-                if r.name.find('{}_ELMo_W/'.format(name)) >= 0
+                if r.name.find('{}_wsum_W/'.format(name)) >= 0
             ]
             if len(reg) != 1:
                 raise ValueError
 
-        # scale the weighted sum by gamma
-        gamma = tf.get_variable(
-            '{}_ELMo_gamma'.format(name),
+        # scale the weighted sum by beta
+        beta = tf.get_variable(
+            '{}_wsum_beta'.format(name),
             shape=(1, ),
             initializer=tf.ones_initializer,
             regularizer=None,
             trainable=True,
         )
-        weighted_lm_layers = sum_pieces * gamma
+        weighted_wsum_layers = sum_pieces * beta
 
-        ret = {'weighted_op': weighted_lm_layers, 'regularization_op': reg}
+        ret = {'weighted_op': weighted_wsum_layers, 'regularization_op': reg}
 
     return ret
